45cb45bfb72f0b3da5312f398aef13e8c253c470
jalmaznaai <jalmaznaai@protonmail.com>
2023-11-23 20:16:05 -0600

    Some testing done

--- src/JavaToUML/JavaToUML.java
+++ src/JavaToUML/JavaToUML.java
@@ -1,6 +1,12 @@
 package JavaToUML;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
 import org.apache.commons.io.FilenameUtils;
 
 public class JavaToUML
@@ -18,6 +24,17 @@ public class JavaToUML
     public boolean hasCorrectSyntax()
     {
         return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        File file = new File("C:\\Users\\aaaaa\\Documents\\Git\\School\\Software Engineering 1\\Project\\" +
+                "Java To UML\\src\\SampleClasses\\Sample.java");
+
+        JavaParser javaParser = new JavaParser();
+        ParseResult<CompilationUnit> cu = javaParser.parse(file);
+
+        System.out.println(cu.isSuccessful());
     }
 }
 
