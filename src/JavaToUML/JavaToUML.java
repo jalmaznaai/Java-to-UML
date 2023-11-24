3c8ee68453ca7ce3bc5f5a016e81caaae3e6e946
jalmaznaai <jalmaznaai@protonmail.com>
2023-11-23 19:46:51 -0600

    Maven now works, plus a few new methods to check if the file given is gonna work

---
+++ src/JavaToUML/JavaToUML.java
@@ -0,0 +1,23 @@
+package JavaToUML;
+
+import java.io.File;
+import org.apache.commons.io.FilenameUtils;
+
+public class JavaToUML
+{
+    public static boolean fileExists(File file)
+    {
+        return (file.exists() && file.isFile());
+    }
+
+    public static boolean isJavaFile(File file)
+    {
+        return FilenameUtils.getExtension(file.getName()).equals("java");
+    }
+
+    public boolean hasCorrectSyntax()
+    {
+        @// TODO: 11/23/2023  
+    }
+}
+
