3ed292d8e4d8ac969d898b6604d0c676d56f5863
jalmaznaai <jalmaznaai@protonmail.com>
2023-11-25 20:23:07 -0600

    Added class Relation class

--- src/JavaToUML/ClassInfo.java
+++ src/JavaToUML/ClassInfo.java
@@ -1,10 +1,14 @@
 package JavaToUML;
 
+import java.util.ArrayList;
+
 public class ClassInfo
 {
     private String ClassName;
     private boolean isAbstract;
     private boolean isInterface;
     private String accessLevel;
+    private ArrayList<Variable> classVariables;
+    private ArrayList<Relation> classRelations;
 
 }
---
+++ src/JavaToUML/Relation.java
@@ -0,0 +1,8 @@
+package JavaToUML;
+
+public class Relation
+{
+    private String relationType;
+    private ClassInfo class1;
+    private ClassInfo class2;
+}
