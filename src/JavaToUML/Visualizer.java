fc513aea21652d660acaa1773f0d5b9bc1ae1f64
jalmaznaai <jalmaznaai@protonmail.com>
2023-11-26 19:18:31 -0600

    Visualizer progress

--- src/JavaToUML/Visualizer.java
+++ src/JavaToUML/Visualizer.java
@@ -1,6 +1,27 @@
 package JavaToUML;
 
+import net.sourceforge.plantuml.SourceFileReader;
+import net.sourceforge.plantuml.GeneratedImage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Visualizer
 {
+    public static void makeImage(HashMap<String, ClassInfo> classes) throws IOException
+    {
+        Date date = new Date();
+        int month;
+        int day;
 
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        makeImage(new HashMap<String, ClassInfo>());
+    }
 }
